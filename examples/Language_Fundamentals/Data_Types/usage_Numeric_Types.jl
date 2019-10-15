331085616b343f29aeb277a8c67f77cc0dafec80
aminya <aminyahyaabadi74@gmail.com>
2019-10-14 21:00:50 -0500

    Use metaprogramming to generate both documentation and code for Numeric_Types.
    Closes https://github.com/juliamatlab/MatLang/issues/8

--- examples/Language_Fundamentals/Data_Types/usage_Numeric_Types.jl
+++ examples/Language_Fundamentals/Data_Types/usage_Numeric_Types.jl
@@ -13,33 +13,33 @@ else
 end
 ################################################################
 ################################################################
-nFloat1 = doubleM(12.0)
-t1 = typeof(nFloat1) # Float64
+nFloat64 = doubleM(12.0)
+t1 = typeof(nFloat64) # Float64
 
-nFloat2 = singleM(12.0)
-t2 = typeof(nFloat2) # Float32
+nFloat32 = singleM(12.0)
+t2 = typeof(nFloat32) # Float32
 
-nInt1 = int8M(-10)
-t3 = typeof(nInt1) # Int8
+nInt8 = int8M(-10)
+t3 = typeof(nInt8) # Int8
 
-nInt2 = int16M(-10)
-t4 = typeof(nInt2) # Int16
+nInt16 = int16M(-10)
+t4 = typeof(nInt16) # Int16
 
-nInt3 = int32M(-10)
-t5 = typeof(nInt3) # Int32
+nInt32 = int32M(-10)
+t5 = typeof(nInt32) # Int32
 
-nInt4 = int64M(-10)
-t6 = typeof(nInt4) # Int64
+nInt64 = int64M(-10)
+t6 = typeof(nInt64) # Int64
 
-nUInt1 = uint8M(10)
-t7 = typeof(nUInt1) # UInt8
+nUInt8 = uint8M(10)
+t7 = typeof(nUInt8) # UInt8
 
-nUInt2 = uint16M(10)
-t8 = typeof(nUInt2) # UInt16
+nUInt16 = uint16M(10)
+t8 = typeof(nUInt16) # UInt16
 
-nUInt3 = uint32M(10)
-t9 = typeof(nUInt3) # UInt32
+nUInt32 = uint32M(10)
+t9 = typeof(nUInt32) # UInt32
 
-nUInt4 = uint64M(10)
-t10 = typeof(nUInt4) # UInt64
+nUInt64 = uint64M(10)
+t10 = typeof(nUInt64) # UInt64
 ################################################################
