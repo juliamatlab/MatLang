f0771e1bb87a45550d98417b03faba5292ed131f
aminya <aminyahyaabadi74@gmail.com>
2019-12-01 06:52:39 -0600

    formatting

--- deps/SnoopCompile/snoopCompile.jl
+++ deps/SnoopCompile/snoopCompile.jl
@@ -1,23 +1,28 @@
 using SnoopCompile
 
 cd(@__DIR__)
+################################################################
 
 ### Log the compiles
-# This only needs to be run once (to generate MatLang.log)
+# This only needs to be run once (to generate log file)
 
-# SnoopCompile.@snoopc MatLangLogPath begin
-SnoopCompile.@snoopc "$(pwd())/MatLang.log" begin
+SnoopCompile.@snoopc "$(pwd())/Snoop.log" begin
+
+    # Use runtests.jl or your exmaples that uses package:
+
     using MatLang, Pkg
+
     include(joinpath(dirname(dirname(pathof(MatLang))), "examples","Language_Fundamentals", "usage_Matrices_and_Arrays.jl"))
     include(joinpath(dirname(dirname(pathof(MatLang))), "examples","Language_Fundamentals", "usage_Entering_Commands.jl"))
     include(joinpath(dirname(dirname(pathof(MatLang))), "examples","Language_Fundamentals", "Data_Types", "usage_Numeric_Types.jl"))
 end
 
+################################################################
+
 ### Parse the compiles and generate precompilation scripts
 # This can be run repeatedly to tweak the scripts
 
-
-data = SnoopCompile.read("$(pwd())/MatLang.log")
+data = SnoopCompile.read("$(pwd())/Snoop.log")
 
 pc = SnoopCompile.parcel(reverse!(data[2]))
 SnoopCompile.write("$(pwd())/precompile", pc)
