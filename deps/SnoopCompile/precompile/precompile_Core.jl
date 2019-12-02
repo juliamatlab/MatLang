dd187eeccc4c097a6b6d46628b8879d4206c5b02
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2019-12-02 11:05:04 +0000

    Update precompile_*.jl file

--- deps/SnoopCompile/precompile/precompile_Core.jl
+++ deps/SnoopCompile/precompile/precompile_Core.jl
@@ -1,7 +1,7 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
-    precompile(Tuple{typeof(Core.Compiler.eltype), Type{Array{Array{Int64, 3}, 1}}})
     precompile(Tuple{typeof(Core.Compiler.eltype), Type{Array{Array{Int64, 1}, 2}}})
+    precompile(Tuple{typeof(Core.Compiler.eltype), Type{Array{Array{Int64, 3}, 1}}})
     precompile(Tuple{typeof(Core.Compiler.eltype), Type{Array{Base.OneTo{Int64}, 1}}})
     precompile(Tuple{typeof(Core.Compiler.eltype), Type{Array{Base.StepRange{Int64, Int64}, 1}}})
 end
