27cc4d95d99a9a1202e57786e04091862d8eb4e7
aminya <aminyahyaabadi74@gmail.com>
2019-12-12 12:41:20 +0330

    move @snoopiBot to a file in the package

--- deps/SnoopCompile/snoopCompile.jl
+++ deps/SnoopCompile/snoopCompile.jl
@@ -1,36 +1,8 @@
-# Input:
-const package = :MatLang
-
-################################################################
-const packageName = string(package)
-const packagePath = joinpath(pwd(),"src","$packageName.jl")
-################################################################
 using SnoopCompile
-################################################################
-const rootPath = pwd()
-precompileDeactivator(packagePath, precompilePath(packageName));
-cd(@__DIR__)
-################################################################
 
-### Log the compiles
-data = @snoopi begin
-
-    using MatLang, Pkg
-
-    # Use runtests.jl
-    # include(joinpath(dirname(dirname(pathof(MatLang))), "test","runtests.jl"))
-
-    # Ues examples
-    include(joinpath(dirname(dirname(pathof(MatLang))), "examples","Language_Fundamentals", "usage_Matrices_and_Arrays.jl"))
-    include(joinpath(dirname(dirname(pathof(MatLang))), "examples","Language_Fundamentals", "usage_Entering_Commands.jl"))
-    include(joinpath(dirname(dirname(pathof(MatLang))), "examples","Language_Fundamentals", "Data_Types", "usage_Numeric_Types.jl"))
+@snoopiBot "MatLang" begin
+  using MatLang, Pkg
+  include(joinpath(dirname(dirname(pathof(MatLang))), "examples","Language_Fundamentals", "usage_Matrices_and_Arrays.jl"))
+  include(joinpath(dirname(dirname(pathof(MatLang))), "examples","Language_Fundamentals", "usage_Entering_Commands.jl"))
+  include(joinpath(dirname(dirname(pathof(MatLang))), "examples","Language_Fundamentals", "Data_Types", "usage_Numeric_Types.jl"))
 end
-
-################################################################
-### Parse the compiles and generate precompilation scripts
-pc = SnoopCompile.parcel(data)
-onlypackage = Dict(package => sort(pc[package]))
-SnoopCompile.write("$(pwd())/precompile",onlypackage)
-################################################################
-cd(rootPath)
-precompileActivator(packagePath, precompilePath(packageName))
