169707ee7c06d6bf062a413d8dede9ab86587164
aminya <aminyahyaabadi74@gmail.com>
2019-12-12 11:11:52 +0330

    using new SnoopCompile for activation and deactivation

--- deps/SnoopCompile/snoopCompile.jl
+++ deps/SnoopCompile/snoopCompile.jl
@@ -1,67 +1,16 @@
+# Input:
 const package = :MatLang
+
 ################################################################
 const packageName = string(package)
 const filePath = joinpath(pwd(),"src","$packageName.jl")
-
-function precompileDeactivator(packageName, filePath)
-
-    file = open(filePath,"r")
-    packageText = read(file, String)
-    close(file)
-
-    available = occursin("include(\"../deps/SnoopCompile/precompile/precompile_$packageName.jl\")", packageText)  && occursin("_precompile_()", packageText)
-
-    if available
-        packageEdited = foldl(replace,
-                     (
-                      "include(\"../deps/SnoopCompile/precompile/precompile_$packageName.jl\")" => "#include(\"../deps/SnoopCompile/precompile/precompile_$packageName.jl\")",
-                      "_precompile_()" => "#_precompile_()",
-                     ),
-                     init = packageText)
-    else
-        error(""" add the following codes into your package:
-         include(\"../deps/SnoopCompile/precompile/precompile_$packageName.jl\")
-         _precompile_()
-         """)
-    end
-
-     file = open(filePath,"w")
-     write(file, packageEdited)
-     close(file)
-end
-
-function precompileActivator(packageName, filePath)
-
-    file = open(filePath,"r")
-    packageText = read(file, String)
-    close(file)
-
-    available = occursin("#include(\"../deps/SnoopCompile/precompile/precompile_$packageName.jl\")", packageText)  && occursin("#_precompile_()", packageText)
-    if available
-        packageEdited = foldl(replace,
-                     (
-                      "#include(\"../deps/SnoopCompile/precompile/precompile_$packageName.jl\")" => "include(\"../deps/SnoopCompile/precompile/precompile_$packageName.jl\")",
-                      "#_precompile_()" => "_precompile_()",
-                     ),
-                     init = packageText)
-    else
-        error(""" add the following codes into your package:
-         include(\"../deps/SnoopCompile/precompile/precompile_$packageName.jl\")
-         _precompile_()
-         """)
-    end
-
-     file = open(filePath,"w")
-     write(file, packageEdited)
-     close(file)
-end
-
-################################################################
-const rootPath = pwd()
-precompileDeactivator(packageName, filePath);
-cd(@__DIR__)
 ################################################################
 using SnoopCompile
+################################################################
+const rootPath = pwd()
+precompileDeactivator(filePath, precompilePath(packageName));
+cd(@__DIR__)
+################################################################
 
 ### Log the compiles
 data = @snoopi begin
@@ -76,6 +25,7 @@ data = @snoopi begin
     include(joinpath(dirname(dirname(pathof(MatLang))), "examples","Language_Fundamentals", "usage_Entering_Commands.jl"))
     include(joinpath(dirname(dirname(pathof(MatLang))), "examples","Language_Fundamentals", "Data_Types", "usage_Numeric_Types.jl"))
 end
+
 ################################################################
 ### Parse the compiles and generate precompilation scripts
 pc = SnoopCompile.parcel(data)
@@ -83,4 +33,4 @@ onlypackage = Dict(package => sort(pc[package]))
 SnoopCompile.write("$(pwd())/precompile",onlypackage)
 ################################################################
 cd(rootPath)
-precompileActivator(packageName, filePath)
+precompileActivator(filePath, precompilePath(packageName))
